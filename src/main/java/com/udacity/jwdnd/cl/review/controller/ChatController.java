af21bf00001daa647a7af7c54b5c4c96d1468dd2
code-vibe <mrsamoladapo@gmail.com>
2022-07-17 23:51:37 +0100

    updated the class to handle Get and Post request

--- src/main/java/com/udacity/jwdnd/cl/review/controller/ChatController.java
+++ src/main/java/com/udacity/jwdnd/cl/review/controller/ChatController.java
@@ -1,2 +1,37 @@
-package com.udacity.jwdnd.cl.review.controller;public class ChatController {
+package com.udacity.jwdnd.cl.review.controller;
+
+import com.udacity.jwdnd.cl.review.model.ChatForm;
+import com.udacity.jwdnd.cl.review.service.MessageService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/chat")
+public class ChatController {
+    private MessageService messageService;
+
+    public ChatController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+    @GetMapping
+    public String getChatPage(ChatForm chatForm, Model model) {
+    model.addAttribute("chatMessages", this.messageService.getChatMessages()) ;
+    return "chat";
+    }
+
+    @PostMapping
+    public String postChatMessage(ChatForm chatForm, Model model) {
+        this.messageService.addMessage(chatForm);
+        chatForm.setMessageText("");
+        model.addAttribute("chatMessages", this.messageService.getChatMessages());
+        return "chat";
+    }
+    @ModelAttribute
+    public String[] allMessageType() {
+        return new String[]{"Say", "Shout", "Whisper"};
+    }
 }
