ca9896655b04b3bf861d1ff735d23cbcd8e3d179
code-vibe <mrsamoladapo@gmail.com>
2022-07-17 23:00:30 +0100

    Updated the MessageService class to have an addMessage method

--- src/main/java/com/udacity/jwdnd/cl/review/service/MessageService.java
+++ src/main/java/com/udacity/jwdnd/cl/review/service/MessageService.java
@@ -1,5 +1,6 @@
 package com.udacity.jwdnd.cl.review.service;
 
+import com.udacity.jwdnd.cl.review.model.ChatForm;
 import com.udacity.jwdnd.cl.review.model.ChatMessage;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +11,29 @@ import java.util.List;
 @Service
 public class MessageService {
     private List<ChatMessage>  chatMessages;
+
     @PostConstruct
     public void postConstruct() {
         System.out.println("Creating MessageService bean");
+        this.chatMessages = new ArrayList<>();
+    }
+
+    public void addMessage(ChatForm chatForm) {
+        ChatMessage newMessage = new ChatMessage();
+        newMessage.setUsername(chatForm.getUsername());
+        switch (chatForm.getMessageType()) {
+            case "Say":
+                newMessage.setMessage(chatForm.getMessageText());
+                break;
+            case "Shout":
+                newMessage.setMessage(chatForm.getMessageText().toUpperCase());
+            case "Whisper":
+                newMessage.setMessage(chatForm.getMessageText().toLowerCase());
+        }
+        this.chatMessages.add(newMessage);
+    }
+
+    public List<ChatMessage> getChatMessages() {
+        return chatMessages;
     }
 }
