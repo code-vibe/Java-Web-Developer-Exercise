da06ef102d199eb56226f06c1752411f7c430c1d
code-vibe <mrsamoladapo@gmail.com>
2022-07-19 12:12:00 +0100

    Added Login support using UserService

---
+++ src/main/java/com/udacity/jwdnd/cl/review/service/UserService.java
@@ -0,0 +1,34 @@
+package com.udacity.jwdnd.cl.review.service;
+
+import com.udacity.jwdnd.cl.review.mapper.UserMapper;
+import com.udacity.jwdnd.cl.review.model.User;
+
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class UserService {
+    private final UserMapper userMapper;
+    private final HashService hashService;
+
+    public UserService(UserMapper userMapper, HashService hashService) {
+        this.userMapper = userMapper;
+        this.hashService = hashService;
+    }
+
+    public boolean isUsernameAvailable(String username) {
+        return userMapper.getUser(username) == null;
+    }
+    public int createUser (User user) {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+        String encodedSalt = Base64.getEncoder().encodeToString(salt);
+        String hashedPassword = hashService.getHashedValue(user.getPassword(), encodedSalt);
+        return userMapper.insert(new User(user.getUsername(), encodedSalt, hashedPassword, user.getFirstName(), user.getLastName(), null));
+    }
+
+
+    public User getUser(String username) {
+        return userMapper.getUser(username);
+    }
+}
